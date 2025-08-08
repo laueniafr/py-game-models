0ae742a6710ac9fd7c4333ac87c1f22409b0b121
Lauenia <laueniafr@outlook.com>
2025-08-08 08:16:14 -0300

    Fixing tipy annotation

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -18,7 +18,7 @@ class Skill(models.Model):
         related_name="skills"
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -26,7 +26,7 @@ class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -48,5 +48,5 @@ class Player(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.nickname
